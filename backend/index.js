0a0e4b01412be7b370cba32a56c312d21b5f0326
Shivang Nagta <141389245+ShivangNagta@users.noreply.github.com>
2024-07-13 02:41:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 
 const app = express();
-const port = process.env.PORT || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const clientOptions = { 
     serverApi: { 
